2064b0313fbb6358e303147422bcd73e6718eefa
bled1908 <bled19082005@gmail.com>
2025-03-05 13:42:48 +0530

    day9-2

--- Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/main.py
+++ Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/main.py
@@ -24,3 +24,28 @@ print(programming_dictionary)
 for thing in programming_dictionary:
     print(thing)
     print(programming_dictionary[thing])
+    
+# Nesting a list and dictionary in a dictionary
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+# Nesting a dictionary in a list
+travel_log = [
+    {
+        "country": "France", 
+        "cities_visited": ["Paris", "Lille", "Dijon"], 
+        "total_visits": 12
+    },
+    {
+        "country": "Germany", 
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+        "total_visits": 5
+    },
+]
