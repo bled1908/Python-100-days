bf81cef6681ab44e7cb7297182cee32e6756fe15
bled1908 <bled19082005@gmail.com>
2025-03-06 12:06:04 +0530

    day10-4

--- Day 10 - Beginner - Functions with Outputs/main.py
+++ Day 10 - Beginner - Functions with Outputs/main.py
@@ -1,4 +1,13 @@
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name.
+
+    Args:
+        f_name (string): First name
+        l_name (string): Last name
+
+    Returns:
+        string: Formatted name
+    """
     if f_name == "" or l_name == "":
         return "You didn't provide valid inputs."
     formatted_f_name = f_name.title()
@@ -6,3 +15,8 @@ def format_name(f_name, l_name):
     return f"{formatted_f_name} {formatted_l_name}"
 
 print(format_name(input("What is your first name? "), input("What is your last name? ")))
+a = """Multi
+line
+String""" #if not assigned to a variable, it will be a multi-line comment but this way of commenting is not preferred
+
+print(a)
