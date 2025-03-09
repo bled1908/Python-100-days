cac029719c3acd113456b022f79b0267db2bc5fb
bled1908 <bled19082005@gmail.com>
2025-03-09 13:14:17 +0530

    day12-main2

--- Day 12  - Beginner - Scope & Number Guessing Game/main.py
+++ Day 12  - Beginner - Scope & Number Guessing Game/main.py
@@ -30,3 +30,11 @@ def game():
 
 game()
 print(player_health)
+
+game_level = 3
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0] 
+        
+    print(new_enemy)
