7a2b5a59619ff3941a9ad1f9a41bc2e6598eac54
bled1908 <bled19082005@gmail.com>
2025-03-09 13:26:00 +0530

    day12-main3

--- Day 12  - Beginner - Scope & Number Guessing Game/main.py
+++ Day 12  - Beginner - Scope & Number Guessing Game/main.py
@@ -1,40 +1,64 @@
 ################### Scope ####################
 
+# enemies = 1
+
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# # Local Scope
+
+# def drink_potion():
+#   potion_strength = 2
+#   print(potion_strength)
+  
+# drink_potion()
+# # print(potion_strength) # This will give an error
+
+# # Global Scope
+# player_health = 10
+
+# def game():
+#     def drink_potion():
+#         potion_strength = 2
+#         print(player_health)
+    
+#     drink_potion()
+
+# game()
+# print(player_health)
+
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alien"]
+#     if game_level < 5:
+#         new_enemy = enemies[0] 
+        
+#     print(new_enemy)
+
+# Modify a global variable
 enemies = 1
 
+# def increase_enemies():
+#     global enemies
+#     enemies += 2
+#     print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# def increase_enemies():
+#     print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
 def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
 
-increase_enemies()
+enemies = increase_enemies()
 print(f"enemies outside function: {enemies}")
-
-# Local Scope
-
-def drink_potion():
-  potion_strength = 2
-  print(potion_strength)
-  
-drink_potion()
-# print(potion_strength) # This will give an error
-
-# Global Scope
-player_health = 10
-
-def game():
-    def drink_potion():
-        potion_strength = 2
-        print(player_health)
-    
-    drink_potion()
-
-game()
-print(player_health)
-
-game_level = 3
-def create_enemy():
-    enemies = ["Skeleton", "Zombie", "Alien"]
-    if game_level < 5:
-        new_enemy = enemies[0] 
-        
-    print(new_enemy)
