4eb9b364e2db6d6acf7a96f4960c6a860da369b5
bled1908 <bled19082005@gmail.com>
2025-03-09 12:34:54 +0530

    day12-main1

---
+++ Day 12  - Beginner - Scope & Number Guessing Game/main.py
@@ -0,0 +1,32 @@
+################### Scope ####################
+
+enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Local Scope
+
+def drink_potion():
+  potion_strength = 2
+  print(potion_strength)
+  
+drink_potion()
+# print(potion_strength) # This will give an error
+
+# Global Scope
+player_health = 10
+
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+    
+    drink_potion()
+
+game()
+print(player_health)
