77d83d9a0d1a0346d077c526120f547f51e4a9f3
bled1908 <bled19082005@gmail.com>
2025-03-13 19:09:13 +0530

    day13-main4

--- Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/main.py
+++ Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/main.py
@@ -15,9 +15,9 @@ print(dice_imgs[dice_num])
 
 # Play Computer
 year = int(input("What's your year of birth? "))
-if year > 1980 and year <= 1994:
+if year > 1980 and year < 1994:
   print("You are a millenial.")
-elif year > 1994:
+elif year >= 1994:
   print("You are a Gen Z.")
 
 # # Fix the Errors
