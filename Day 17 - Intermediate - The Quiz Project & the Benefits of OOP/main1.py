ba778318b293a51c6f26a68aea0d43c221335f8e
bled1908 <bled19082005@gmail.com>
2025-03-30 10:18:58 +0530

    day17-4

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main1.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main1.py
@@ -8,11 +8,18 @@ class User: #Pascal case for class names
         self.username = username
         self.password = password
         self.followers = 0 # default value for followers
-
+        self.following = 0
+    
+    def follow(self, user):
+        # method is a function that is associated with an object
+        user.followers += 1
+        self.following += 1
+        
+        
 user_1 = User("001", "Subha", "1234") #Creating an object of the class User
 
-print(user_1.username) # subha
-print(user_1.followers) # 0
+# print(user_1.username) # subha
+# print(user_1.followers) # 0
 
 user_2 = User("002", "Riya", "5678") 
 # can also do this:
@@ -21,7 +28,13 @@ user_2 = User("002", "Riya", "5678")
 # user_2.username = "Riya"
 # user_2.password = "5678"
 
-print(user_2.username) # Riya
+# print(user_2.username) # Riya
 
 # def function():
 #     pass # This is a placeholder function
+
+user_1.follow(user_2)
+print(user_1.followers) # 0
+print(user_1.following) # 1
+print(user_2.followers) # 1
+print(user_2.following) # 0
