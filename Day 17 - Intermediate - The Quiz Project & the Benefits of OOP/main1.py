4010ee011d6f604be829cb41b603f9a4b3ea73e2
bled1908 <bled19082005@gmail.com>
2025-03-30 09:57:20 +0530

    day17-3

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main1.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main1.py
@@ -1,22 +1,25 @@
 class User: #Pascal case for class names
     
-    def __init__(self):
+    def __init__(self, user_id, username, password): #Constructor method
         # Constructor method to initialize attributes
         print("New user being created...")
-        self.username = None
-        self.password = None
+        #attribute is a variable associated with a class and object
+        self.id = user_id # self is a reference to the current instance of the class
+        self.username = username
+        self.password = password
+        self.followers = 0 # default value for followers
 
-user_1 = User()
-user_1.id = "001" #attribute is a variable associated with a class and object
-user_1.username = "subha"
-user_1.password = "password123"
+user_1 = User("001", "Subha", "1234") #Creating an object of the class User
 
 print(user_1.username) # subha
+print(user_1.followers) # 0
 
-user_2 = User()
-user_2.id = "002"
-user_2.username = "Riya"
-user_2.password = "password456"
+user_2 = User("002", "Riya", "5678") 
+# can also do this:
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "Riya"
+# user_2.password = "5678"
 
 print(user_2.username) # Riya
 
