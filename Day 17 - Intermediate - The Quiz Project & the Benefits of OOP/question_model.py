658a92a332d58d8a2cdc8ee16748bf257fa84d26
bled1908 <bled19082005@gmail.com>
2025-03-30 10:28:22 +0530

    day17-5

---
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    """A class to create a question object with text and answer attributes."""
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+        
