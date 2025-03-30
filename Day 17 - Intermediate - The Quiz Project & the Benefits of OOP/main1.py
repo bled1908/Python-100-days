a6ef3523d13f606c840461e8cafe49bffa3cde71
bled1908 <bled19082005@gmail.com>
2025-03-30 09:46:52 +0530

    day17-2

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main1.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main1.py
@@ -1,7 +1,24 @@
 class User: #Pascal case for class names
     
+    def __init__(self):
+        # Constructor method to initialize attributes
+        print("New user being created...")
+        self.username = None
+        self.password = None
 
 user_1 = User()
+user_1.id = "001" #attribute is a variable associated with a class and object
+user_1.username = "subha"
+user_1.password = "password123"
+
+print(user_1.username) # subha
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Riya"
+user_2.password = "password456"
+
+print(user_2.username) # Riya
 
 # def function():
 #     pass # This is a placeholder function
