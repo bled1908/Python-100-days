cd30601841f65b31bfb267186ff8213e37f23a8f
bled1908 <bled19082005@gmail.com>
2025-04-20 15:06:09 +0530

    day20-2

--- Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/main.py
+++ Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,26 +8,13 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)  # Turns off the screen updates
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-# screen.update()  # Update the screen to show the initial segments
+snake = Snake()
     
 game_is_on = True
 while game_is_on:
     screen.update()  # Update the screen after each segment moves
     time.sleep(0.1) # Control the speed of the game
     
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-        
-    segments[0].forward(20)  # Move the first segment forward
+    snake.move()
 
 screen.exitonclick() 
