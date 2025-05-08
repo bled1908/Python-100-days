8380783fd5e60ec15e7ea18cd6cd635ec498cf5a
bled1908 <bled19082005@gmail.com>
2025-05-08 21:33:17 +0530

    end_day21

--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/food.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/food.py
@@ -5,10 +5,10 @@ class Food(Turtle):
     
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.color("gold")
         self.speed("fastest")
         self.refresh()
         
--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/notes.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/notes.py
@@ -26,3 +26,19 @@ nemo = Fish()
 nemo.swim()
 nemo.breathe()
 print(nemo.num_eyes)
+
+# Slicing of lists
+# List slicing is a way to access a subset of elements from a list. It allows you to create a new list that contains a portion of the original list. The syntax for slicing is as follows:
+# list[start:stop:step]
+
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+print(piano_keys[0:3])
+print(piano_keys[0:7:2]) # start at 0, go to 7, take every 2nd element
+print(piano_keys[::2]) # start at 0, go to end, take every 2nd element
+print(piano_keys[::-1]) # start at end, go to 0, take every 2nd element (reverse the list)
+print(piano_keys[::-2]) # start at end, go to 0, take every 2nd element (reverse the list and take every 2nd element)
+print(piano_keys[2:]) # start at 2, go to end (take everything from 2 to end)
+print(piano_keys[:3]) # start at 0, go to 3 (take everything from 0 to 3)
+
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+print(piano_tuple[0:3])
