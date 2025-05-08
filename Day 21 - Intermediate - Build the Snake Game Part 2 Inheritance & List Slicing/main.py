b9505a8e3fd3793d2aa466ee2ca5b8b417d9eb41
bled1908 <bled19082005@gmail.com>
2025-05-08 21:05:21 +0530

    day21_3

--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/main.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)  # Turns off the screen updates
 
 snake = Snake()
 food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,6 +22,7 @@ screen.onkey(snake.right, "Right")
     
 game_is_on = True
 while game_is_on:
+    score.update_scoreboard()
     screen.update()  # Update the screen after each segment moves
     time.sleep(0.1) # Control the speed of the game
     snake.move()
@@ -27,5 +30,6 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        score.increase_score()
 
 screen.exitonclick() 
