f4408064b948dbd5b4c6f062d25e981ae7240c42
bled1908 <bled19082005@gmail.com>
2025-06-15 18:04:50 +0530

    create ball class and make ball move

--- Day 22 - Intermediate - Build Pong The Famous Arcade Game/main.py
+++ Day 22 - Intermediate - Build Pong The Famous Arcade Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
-from paddle import Paddle  # Assuming you have a paddle.py file with Paddle class defined
+from paddle import Paddle 
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)  # Turns off the screen updates
 
 r_paddle = Paddle(350, 0)  # Right paddle
 l_paddle = Paddle(-350, 0)  # Left paddle
+ball = Ball()
     
 screen.listen()
 screen.onkey(r_paddle.move_up, "Up")
@@ -18,8 +21,10 @@ screen.onkey(l_paddle.move_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)  # Control the speed of the game
     screen.update()  # Update the screen after each segment moves
     # Here you would typically add logic for the ball movement, collision detection, etc.
+    ball.move()
     
     # For now, we will just keep the game running
     pass
